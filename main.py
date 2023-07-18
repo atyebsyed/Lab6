e8c5dd7c1ebb7ae6f50652393f9b6432fd346e5c
Syed Jawed <syedjawed@ufl.edu>
2023-07-18 16:18:07 -0400

    version control lab6

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-# first github edition
-print('hello world')
+# Syed Jawed
+
+# password encoder
+def encoder(password):
+    new = ''
+    for i in password:
+        data = ((int(i) + 3) % 10)
+        new += data
+    return new
+
+# password decoder
+def decoder(password):
+
+
+def main():
+    while True:
+        # print all the menu options
+        print('\nRLE Menu')
+        print('-' * 13)
+        print('1. Encode\n2. Decode\n3. Quit')
+        # Prompt user for menu option
+        option = int(input("\nPlease enter an option: "))
+        # option 1, 2 ,3
+        if option == 1:
+            password = input('Please enter your password to encode: ')
+            new = encoder(password)
+            print('Your password has been encoded and stored!')
+            pass
+        elif option == 2:
+            old = decoder(password)
+            print('The encoded password is', new, 'and the original password is', old'.')
+            pass
+        elif option == 3:
+            break
+
+if __name__ == '__main__':
+    main()
